a1a19603dd619b10d49c5d048456244d48cc4928
Shivganesh <shivmarkad99@gmail.com>
2023-09-27 16:17:42 +0530

    [Shivganesh] Added: aggregation method group books

--- src/services/book.service.js
+++ src/services/book.service.js
@@ -2,14 +2,20 @@ import Book from '../models/books.model';
 
 //get all books
 export const getBooks = async () => {
-  const data = await Book.aggregate([
-    { $match : {price:{$gte: 100}}}]);
+  const data = await Book.aggregate([{ $match: { price: { $gte: 100 } } }]);
   return data;
 };
 
 //get all books
 export const getDiscBooks = async () => {
+  
   const data = await Book.aggregate([
-    { $match : {price:{$lte: 500},discountPrice: {$gte: 100}}}]);
+    {
+      $match: { price: { $lte: 500 }, discountPrice: { $gte: 100 } }
+    },
+      {
+      $group: { _id: "$price",totalPrice: { $sum:"$price" } }
+    }
+  ]);
   return data;
 };
