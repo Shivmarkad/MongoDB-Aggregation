5b6a51bb21f04feb61ee8114056c6c2d56e93ffd
Shivganesh <shivmarkad99@gmail.com>
2023-09-27 15:04:56 +0530

    [Shivganesh] Added: fetching books using aggregation which price greater than 100rs

--- src/services/book.service.js
+++ src/services/book.service.js
@@ -3,6 +3,6 @@ import Book from '../models/books.model';
 //get all users
 export const getBooks = async () => {
   const data = await Book.aggregate([
-    { $match : {price:{$gte: 500}}}]);
+    { $match : {price:{$gte: 100}}}]);
   return data;
 };
